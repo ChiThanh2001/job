30675a32dff5db174fa919aff083bec4567f615f
ChiThanh2001 <satthumaulanh2001@gmail.com>
2022-11-16 00:10:25 +0700

    fix bug

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import AreaChart from "./AreaChart"
+import BarChart from "./BarChart"
+import {useState} from 'react'
+import { useAppContext } from '../context/appContext'
+import Wrapper from '../assets/wrappers/ChartsContainer'
 
-const ChartsContainer = () => {
+export default function ChartsContainer(){
+  const [barChart, setBarChart] = useState(true)
+
+  const { monthlyApplications: data } = useAppContext()
   return (
-    <div>
-      Chart Container
-    </div>
+    <Wrapper>
+      <h4>Monthly Application</h4>
+      <button type='button' onClick= {()=> setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} /> }
+    </Wrapper>
   )
 }
 
-export default ChartsContainer
--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -17,46 +17,42 @@ const createJob = async (req, res) => {
 
 const getAllJobs = async (req, res) => {
     const { search, status, jobType, sort } = req.query;
-
+  
     const queryObject = {
-        createdBy: req.user.userId,
+      createdBy: req.user.userId,
     };
-
+  
     if (status !== 'all') {
-        queryObject.status = status;
+      queryObject.status = status;
     }
     if (jobType !== 'all') {
-        queryObject.jobType = jobType;
+      queryObject.jobType = jobType;
     }
-
     if (search) {
-        queryObject.position = { $regex: search, $options: 'i' };
-      }
+      queryObject.position = { $regex: search, $options: 'i' };
+    }
     // NO AWAIT
     let result = Job.find(queryObject);
-
+  
     // chain sort conditions
-    if(sort === 'latest'){
-        result = result.sort('-createdAt')
+    if (sort === 'latest') {
+      result = result.sort('-createdAt');
     }
-
-    if(sort === 'oldest'){
-        result = result.sort('createdAt')
+    if (sort === 'oldest') {
+      result = result.sort('createdAt');
     }
-
-    if(sort === 'a-z'){
-        result = result.sort('position')
+    if (sort === 'a-z') {
+      result = result.sort('position');
     }
-
-    if(sort === 'z-a'){
-        result = result.sort('-position')
+    if (sort === 'z-a') {
+      result = result.sort('-position');
     }
     const jobs = await result;
-
+  
     res
-        .status(StatusCodes.OK)
-        .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
-};
+      .status(StatusCodes.OK)
+      .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+  };
 
 const updateJob = async (req, res) => {
     const { id: jobId } = req.params
